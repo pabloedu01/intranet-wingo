23b40465ac94ef664c971aa4d0399479202a213a
pabloedu01 <pablo@pabloedu.com>
2023-05-19 15:37:37 -0300

    inicial

---
+++ breadcrumb.php
@@ -0,0 +1,20 @@
+<div class="page-body">
+    <div class="container-fluid">
+        <div class="page-header dash-breadcrumb">
+            <div class="row">
+                <div class="col-6">
+                    <h3 class="f-w-600">default</h3>
+                </div>
+                <div class="col-6">
+                    <div class="breadcrumb-sec">
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html"><i data-feather="home"></i></a></li>
+                            <li class="breadcrumb-item"><?php echo "$nome_empresa" ?></li>
+                            <li class="breadcrumb-item active">Default</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
