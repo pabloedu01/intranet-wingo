6a84a522a74d22011237dc43746aaa19385bdb9e
pabloedu01 <pablo@pabloedu.com>
2023-05-19 16:00:12 -0300

    teste

--- api/breadcrumb.php
+++ api/breadcrumb.php
@@ -9,7 +9,7 @@
                     <div class="breadcrumb-sec">
                         <ul class="breadcrumb">
                             <li class="breadcrumb-item"><a href="index.html"><i data-feather="home"></i></a></li>
-                            <li class="breadcrumb-item"><?php echo "$nome_empresa" ?></li>
+                            <li class="breadcrumb-item"><?php echo "$nome_empresa" ;?></li>
                             <li class="breadcrumb-item active">Default</li>
                         </ul>
                     </div>
